0aff91e4ad6bec840011bd61865b20176fa1c604
Pupochek <alar.akilbekov99@gmail.com>
2022-02-19 19:10:03 +0600

    pre final

---
+++ scripts/biba.js
@@ -0,0 +1,20 @@
+let bibajs = () => {
+  let acc = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      let panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+};
+bibajs();
